2f1213f021f322ad18e799bb54c0cbf5ef83f28c
evgenirusev <evgeni.n.rusev@gmail.com>
2024-05-28 21:24:42 +0300

    Add scrutor and register services

---
+++ Template.Domain/DomainConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+public static class DomainConfiguration
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services)
+        => services
+            .Scan(scan => scan
+                .FromCallingAssembly()
+                .AddClasses(classes => classes
+                    .AssignableTo(typeof(IFactory<>)))
+                .AsMatchingInterface()
+                .WithTransientLifetime());
+}
