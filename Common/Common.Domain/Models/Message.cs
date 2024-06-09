1f5eff9ff2927c15520f4ba8dbab373c4d1e1940
evgenirusev <evgeni.n.rusev@gmail.com>
2024-06-09 12:20:02 +0300

    add message and configuration

---
+++ Common/Common.Domain/Models/Message.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+public class Message
+{
+    private string serializedData = default!;
+
+    public Message(object data) 
+    {
+        Data = data;
+    }
+
+    private Message()
+    {
+    }
+
+    public int Id { get; private set; }
+
+    public Type Type { get; private set; } = default!;
+
+    public bool Published { get; private set; }
+
+    public void MarkAsPublished() => Published = true;
+
+    public object Data
+    {
+        get
+        {
+            return JsonSerializer.Deserialize(serializedData, Type, new JsonSerializerOptions
+            {
+                IgnoreNullValues = true
+            });
+        }
+        set
+        {
+            Type = value.GetType();
+            serializedData = JsonSerializer.Serialize(value, new JsonSerializerOptions
+            {
+                IgnoreNullValues = true
+            });
+        }
+    }
+}
---
+++ Common/Common.Infrastructure/Configuration/MessageConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+internal class MessageConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder
+            .HasKey(m => m.Id);
+
+        builder
+            .Property<string>("serializedData")
+            .IsRequired()
+            .HasField("serializedData");
+
+        builder
+            .Property(m => m.Type)
+            .IsRequired()
+            .HasConversion(
+                t => t.AssemblyQualifiedName,
+                t => Type.GetType(t!)!);
+
+        builder
+            .Ignore(m => m.Data);
+    }
+}
