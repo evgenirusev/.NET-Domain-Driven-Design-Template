53cbf64b57c3fb72725f80d8fbbd151aeeb960ed
evgenirusev <evgeni.n.rusev@gmail.com>
2024-06-13 20:45:34 +0300

    Update product command

--- Products/Products.Application/Products/Commands/Update/UpdateProductCommand.cs
+++ Products/Products.Application/Products/Commands/Update/UpdateProductCommand.cs
@@ -1,8 +1,10 @@
 using MediatR;
 
-public class UpdateProductCommand : ProductCommand, IRequest<Unit>
+public class UpdateProductCommand : ProductCommand, IRequest<Result>
 {
-    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, Unit>
+    public int Id { get; set; }
+    
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, Result>
     {
         private readonly IProductDomainRepository productRepository;
 
@@ -11,19 +13,19 @@ public class UpdateProductCommand : ProductCommand, IRequest<Unit>
             this.productRepository = productRepository;
         }
 
-        public async Task<Unit> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        public async Task<Result> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
         {
-            // var product = await productRepository.Find(request.Id, cancellationToken);
-            //
-            // product.UpdateName(request.Name)
-            //     .UpdateDescription(request.Description)
-            //     .UpdateProductType(Enumeration.FromValue<ProductType>(request.ProductType))
-            //     .UpdatePrice(request.Price.Amount, request.Price.Currency)
-            //     .UpdateWeight(request.Weight.Value, request.Weight.Unit);
-            //
-            // await productRepository.Save(product, cancellationToken);
+            var product = await productRepository.Find(request.Id, cancellationToken);
 
-            return Unit.Value;
+            product.UpdateName(request.Name)
+                .UpdateDescription(request.Description)
+                .UpdateProductType(Enumeration.FromValue<ProductType>(request.ProductType))
+                .UpdatePrice(request.Price.Amount, request.Price.Currency)
+                .UpdateWeight(request.Weight.Value, request.Weight.Unit);
+
+            await productRepository.Save(product, cancellationToken);
+
+            return Result.Success;
         }
     }
 }
--- Products/Products.Web/Features/ProductsController.cs
+++ Products/Products.Web/Features/ProductsController.cs
@@ -5,12 +5,15 @@ public class ProductsController : ApiController
 {
     [HttpGet]
     [Route(Id)]
-    public async Task<ActionResult<ProductResponse>> GetById(
-        [FromRoute] ProductDetailsQuery query)
+    public async Task<ActionResult<ProductResponse>> GetById([FromRoute] ProductDetailsQuery query)
         => await Send(query);
     
     [HttpPost]
-    public async Task<ActionResult<CreateProductResponse>> Create(
-        CreateProductCommand command)
+    public async Task<ActionResult<CreateProductResponse>> Create(CreateProductCommand command)
+        => await Send(command);
+    
+    [HttpPut]
+    [Route(Id)]
+    public async Task<ActionResult> Update(UpdateProductCommand command)
         => await Send(command);
 }
