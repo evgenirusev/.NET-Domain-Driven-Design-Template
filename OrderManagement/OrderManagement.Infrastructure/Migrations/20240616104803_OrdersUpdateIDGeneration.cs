5629f23af14540ca343869632956af069cc0126e
evgenirusev <evgeni.n.rusev@gmail.com>
2024-06-16 13:48:19 +0300

    Add migrations for id generation

---
+++ OrderManagement/OrderManagement.Infrastructure/Migrations/20240616104803_OrdersUpdateIDGeneration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orders.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrdersUpdateIDGeneration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CustomerId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
---
+++ Statistics/Statistics.Infrastructure/Migrations/20240616104650_StatisticsUpdateIDGeneration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Statistics.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class StatisticsUpdateIDGeneration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
