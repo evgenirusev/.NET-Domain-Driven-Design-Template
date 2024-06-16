f39908937013032c0a124beccb2ebf44035793ac
evgenirusev <evgeni.n.rusev@gmail.com>
2024-06-16 12:29:07 +0300

    Statistics web configuration

---
+++ Statistics/Statistics.Application/StatisticsApplicationConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class StatisticsApplicationConfiguration
+{
+    private static readonly Assembly Assembly = Assembly.GetExecutingAssembly();
+
+    public static IServiceCollection AddStatisticsCatalogApplication(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        => services
+            .AddCommonApplication(configuration, Assembly);
+}   
---
+++ Statistics/Statistics.Web/WebConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+public static class WebConfiguration
+{
+    public static IServiceCollection AddStatisticsWebComponents(
+        this IServiceCollection services)
+        => services.AddWebComponents(
+            typeof(StatisticsApplicationConfiguration));
+}
